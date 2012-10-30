2f0b4881cae7fd8d7e902dc70a1ca6b30094272d
Marcelo Silveira <marcelo@mhfs.com.br>
2012-10-30 19:18:37 -0200

    bump to 0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## Unreleased
+
+## 0.0.2
+
+  * Show backtrace similar to resque (Kunal Modi)
--- lib/sidekiq/failures/version.rb
+++ lib/sidekiq/failures/version.rb
@@ -1,5 +1,5 @@
 module Sidekiq
   module Failures
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
