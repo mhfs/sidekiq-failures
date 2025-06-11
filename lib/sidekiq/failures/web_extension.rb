63252253b1a17b7115fe086a910881467cd0e55d
Navid EMAD <navid.emad@yespark.fr>
2025-06-11 23:22:21 +0200

    Update for Sidekiq 8.0 compatibility while maintaining backward compatibility

--- lib/sidekiq/failures/web_extension.rb
+++ lib/sidekiq/failures/web_extension.rb
@@ -1,51 +1,130 @@
 module Sidekiq
   module Failures
     module WebExtension
+      LEGACY_SIDEKIQ_VERSION = Gem::Version.new("7.3.9")
+
+      # Helper method to check Sidekiq version
+      def self.legacy_sidekiq?
+        Gem::Version.new(Sidekiq::VERSION) <= LEGACY_SIDEKIQ_VERSION
+      end
+
+      # Helper method to get parameters based on path and parameter name
+      def self.fetch_param_value(path, param_name)
+        if legacy_sidekiq?
+          # For newer Sidekiq, use route_params or url_params based on path
+          lambda { |env| env.params[param_name] }
+        else
+          # For legacy Sidekiq, just use params
+          if path.include?(":#{param_name}")
+            lambda { |env| env.route_params(param_name.to_sym) }
+          else
+            lambda { |env| env.url_params(param_name.to_s) }
+          end
+        end
+      end
+
+      # Helper method to handle parse_params vs parse_key compatibility
+      def self.parse_key_or_params
+        lambda do |env, key|
+          if env.respond_to?(:parse_key)
+            env.parse_key(key)
+          else
+            env.parse_params(key)
+          end
+        end
+      end
+
+      # Define the helper method implementation that will be used in both versions
+      # Instead of a static lambda, we'll return a method that needs to be evaluated in context
+      def self.safe_relative_time_implementation
+        lambda do |time, context|
+          return unless time
+
+          time = if time.is_a?(Numeric)
+            Time.at(time)
+          else
+            Time.parse(time)
+          end
+
+          # Use the context to call relative_time
+          context.relative_time(time)
+        end
+      end
+
+      # Define the helpers module for Sidekiq 8.0+
+      module FailuresHelpers
+        def safe_relative_time(time)
+          # Pass self (the context with relative_time) to the implementation
+          WebExtension.safe_relative_time_implementation.call(time, self)
+        end
+      end
 
       def self.registered(app)
         view_path = File.join(File.expand_path("..", __FILE__), "views")
+        if legacy_sidekiq?
+          failures_view_path = File.join(view_path, "failures_legacy.erb")
+          failure_view_path = File.join(view_path, "failure_legacy.erb")
+        else
+          failures_view_path = File.join(view_path, "failures.erb")
+          failure_view_path = File.join(view_path, "failure.erb")
+        end
 
-        app.helpers do
-          def safe_relative_time(time)
-            return unless time
+        # Create a parse helper for use in routes
+        parse_helper = parse_key_or_params
 
-            time = if time.is_a?(Numeric)
-              Time.at(time)
-            else
-              Time.parse(time)
+        # Use appropriate helpers implementation based on Sidekiq version
+        if legacy_sidekiq?
+          # Original implementation for older Sidekiq versions
+          app.helpers do
+            define_method(:safe_relative_time) do |time|
+              # Pass self (the context with relative_time) to the implementation
+              WebExtension.safe_relative_time_implementation.call(time, self)
             end
-
-            relative_time(time)
           end
+        else
+          # New implementation for Sidekiq 8.0+
+          app.helpers(FailuresHelpers)
         end
 
         app.get "/failures" do
-          @count = (params[:count] || 25).to_i
-          (@current_page, @total_size, @failures) = page(LIST_KEY, params[:page], @count, :reverse => true)
+          page_param = Sidekiq::Failures::WebExtension.fetch_param_value("/failures", "page").call(self)
+          count_param = Sidekiq::Failures::WebExtension.fetch_param_value("/failures", "count").call(self)
+          @count = (count_param || 25).to_i
+
+          (@current_page, @total_size, @failures) = page(LIST_KEY, page_param, @count, :reverse => true)
           @failures = @failures.map {|msg, score| Sidekiq::SortedEntry.new(nil, score, msg) }
 
-          render(:erb, File.read(File.join(view_path, "failures.erb")))
+          render(:erb, File.read(failures_view_path))
         end
 
         app.get "/failures/:key" do
-          halt 404 unless params['key']
+          key_param = Sidekiq::Failures::WebExtension.fetch_param_value("/failures/:key", "key").call(self)
+          halt 404 unless key_param
 
-          @failure = FailureSet.new.fetch(*parse_params(params['key'])).first
-          redirect "#{root_path}failures" if @failure.nil?
-          render(:erb, File.read(File.join(view_path, "failure.erb")))
+          @failure = FailureSet.new.fetch(*parse_helper.call(self, key_param)).first
+          if @failure.nil?
+            redirect "#{root_path}failures"
+          else
+            render(:erb, File.read(failure_view_path))
+          end
         end
 
         app.post "/failures" do
-          halt 404 unless params['key']
+          key_param = Sidekiq::Failures::WebExtension.fetch_param_value("/failures", "key").call(self)
+          halt 404 unless key_param
 
-          params['key'].each do |key|
-            job = FailureSet.new.fetch(*parse_params(key)).first
+          key_param.each do |key|
+            job = FailureSet.new.fetch(*parse_helper.call(self, key)).first
             next unless job
 
-            if params['retry']
+            retry_param = Sidekiq::Failures::WebExtension.fetch_param_value("/failures", "retry").call(self)
+            if retry_param
               job.retry_failure
-            elsif params['delete']
-              job.delete
+            else
+              delete_param = Sidekiq::Failures::WebExtension.fetch_param_value("/failures", "delete").call(self)
+              if delete_param
+                job.delete
+              end
             end
           end
 
@@ -53,14 +132,19 @@ module Sidekiq
         end
 
         app.post "/failures/:key" do
-          halt 404 unless params['key']
+          key_param = Sidekiq::Failures::WebExtension.fetch_param_value("/failures/:key", "key").call(self)
+          halt 404 unless key_param
 
-          job = FailureSet.new.fetch(*parse_params(params['key'])).first
+          job = FailureSet.new.fetch(*parse_helper.call(self, key_param)).first
           if job
-            if params['retry']
+            retry_param = Sidekiq::Failures::WebExtension.fetch_param_value("/failures/:key", "retry").call(self)
+            if retry_param
               job.retry_failure
-            elsif params['delete']
-              job.delete
+            else
+              delete_param = Sidekiq::Failures::WebExtension.fetch_param_value("/failures/:key", "delete").call(self)
+              if delete_param
+                job.delete
+              end
             end
           end
           redirect_with_query("#{root_path}failures")
@@ -86,10 +170,11 @@ module Sidekiq
         end
 
         app.post '/filter/failures' do
-          @failures = Sidekiq::Failures::FailureSet.new.scan("*#{params[:substr]}*")
+          substr_param = Sidekiq::Failures::WebExtension.fetch_param_value("/filter/failures", "substr").call(self)
+          @failures = Sidekiq::Failures::FailureSet.new.scan("*#{substr_param}*")
           @current_page = 1
           @count = @total_size = @failures.count
-          render(:erb, File.read(File.join(view_path, "failures.erb")))
+          render(:erb, File.read(failures_view_path))
         end
       end
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
 $TESTING = true
 
+ENV["MT_CPU"] = "1" # Disable parallel testing to avoid flaky tests, force a single CPU for minitest
+
 require "minitest/autorun"
 require "minitest/spec"
 require "minitest/mock"
@@ -15,4 +17,4 @@ require "sidekiq/cli"
 
 Sidekiq.logger.level = Logger::ERROR
 
-REDIS = Sidekiq::RedisConnection.create(url: "redis://127.0.0.1:6379/0")
+REDIS = Sidekiq::RedisConnection.create(url: "redis://127.0.0.1:6379/15") # Use DB 15 for testing
